1368b867766b249372b978a08860c785b502371a
tarunfy <tarunsharma8920@gmail.com>
2022-04-15 23:01:19 +0530

    created navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,42 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            alt="telephone"
+            width="32"
+            height="32"
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>Order Now!</div>
+          <div className={styles.text}>0120 894 839</div>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="logo" width="160px" height="69px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="logo" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
