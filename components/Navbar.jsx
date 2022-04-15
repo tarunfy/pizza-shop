e31c0a702a366c44e769221a998038ccae2d2381
tarunfy <tarunsharma8920@gmail.com>
2022-04-15 20:43:31 +0530

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
+};
+
+export default Navbar;
